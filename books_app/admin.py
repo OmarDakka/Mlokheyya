9d6a6c0db75778e0ecc465958dccc9456ddc313e
Omar Daqah <omardakka42@gmail.com>
2021-06-03 00:36:48 +0300

    finished the reg/login page

--- books_app/admin.py
+++ books_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from books_app.models import Book
+from books_app.models import Comment
+from books_app.models import Category
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Comment)
+admin.site.register(Category)
--- users_app/urls.py
+++ users_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('', views.home),
+    path('',views.index),
+    path('reg',views.registration),
+    path('logout',views.logout),
+    path('login',views.login),
+    path('welcome',views.welcome),
 ]
