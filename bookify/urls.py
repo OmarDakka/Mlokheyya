7ac6b9006cdfde0d8c863d38c860f533eadbb3e3
Tasneem Arafat <tasneem.arafat@gmail.com>
2021-06-03 13:33:46 +0300

    updating html files deletenig the base and extend

--- bookify/urls.py
+++ bookify/urls.py
@@ -20,5 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('users_app.urls')),
-    path('books',include('books_app.urls'))
+    path('books/',include('books_app.urls'))
 ]
