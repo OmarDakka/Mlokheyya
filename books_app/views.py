382dd117acfd85f79fbbdf956467d2f416942df4
Adel-Rimawi <a.s.riimawi@gmail.com>
2021-06-03 11:44:51 +0300

    views

--- books_app/views.py
+++ books_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render (request,'About_us.html')
+
+def category(request):
+    return render (request,'Category.html')
+
+
+def book(request):
+    return render (request,'book.html')
+
+def user_page(request):
+    return render (request,'user_page.html')
+
+
+
+def home(request):
+    return render (request,'home.html')
+
